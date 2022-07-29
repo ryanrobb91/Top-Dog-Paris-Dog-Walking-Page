97ecb6185e18ca8f7c8b9a498339db895f4ecd1d
Ryan Robb <Ryan.robb24@gmail.com>
2022-07-28 22:30:27 -0400

    edited layout,need to finish cart and contact form

---
+++ js/products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Morning Walk",
+      desc : "1hr.",
+      img : "img/chris-dog-walking-pic.jpg",
+      price : 20
+    },
+    124: {
+      name : "Moring Walk",
+      desc : "2hr",
+      img : "img/chris-dog-walking-pic.jpg",
+      price : 30
+    },
+    125: {
+      name : "Evening Walk",
+      desc : "1hr",
+      img : "img/dog-walking-evening.jpg",
+      price : 30
+    },
+    126: {
+      name : "Evening Walk",
+      desc : "2hr",
+      img : "img/dog-walking-evening.jpg",
+      price : 50
+    }
+  };
